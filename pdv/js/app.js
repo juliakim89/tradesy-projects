16e25b871d914bbc472fa508884e7a90df1954f8
juliakim89 <juliakim89@gmail.com>
2015-06-23 09:27:14 -0700

    pdv

--- pdv/js/app.js
+++ pdv/js/app.js
@@ -1,3 +1,24 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(window).load(function() {
+  // The slider being synced must be initialized first
+  $('#idp-thumbnails').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 90,
+    itemMargin: 10,
+    asNavFor: '#idp-image-main'
+  });
+ 
+  $('#idp-image-main').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#idp-thumbnails"
+  });
+});
